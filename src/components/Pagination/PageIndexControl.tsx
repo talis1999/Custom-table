fd919a3b7d80fcf84eb6fde22def44366dba3bb0
talis1999 <dantalisman@gmail.com>
2023-10-25 01:11:12 +0300

    adding go to option for page index control

--- src/components/Pagination/PageIndexControl.tsx
+++ src/components/Pagination/PageIndexControl.tsx
@@ -6,6 +6,7 @@ import NextIcon from "@mui/icons-material/ChevronRightTwoTone";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { getPage, selectPagesLength, setPage } from "../../features/data/data";
 import useDebounce from "../../hooks/useDebounce";
+import PageSelect from "./PageSelect";
 
 const PageIndexControl: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -13,8 +14,8 @@ const PageIndexControl: React.FC = () => {
   const currentPage = useAppSelector(getPage);
   const currentPagesLength = useAppSelector(selectPagesLength);
 
-  const [pageIndexValue, setPageIndexValue] = useState(currentPage);
-  const debouncedPageIndexValue = useDebounce(pageIndexValue);
+  const [pageIndexValue, setPageIndexValue] = useState<number>(currentPage);
+  const debouncedPageIndexValue = useDebounce<number>(pageIndexValue);
 
   // Handling dispatch after proper debounce
   useEffect(() => {
@@ -43,7 +44,9 @@ const PageIndexControl: React.FC = () => {
       >
         <PrevIcon />
       </IconButton>
-      <Box>{`Page ${pageIndexValue} of ${currentPagesLength}`}</Box>
+      <Box>{"page"}</Box>
+      <PageSelect page={pageIndexValue} goToPage={setPageIndexValue} />
+      <Box>{`of ${currentPagesLength}`}</Box>
       <IconButton
         aria-label="next"
         onClick={goNext}
---
+++ src/components/Pagination/PageSelect.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from "react";
+import { MenuItem, FormControl } from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import { useAppSelector } from "../../app/hooks";
+import { selectPagesLength } from "../../features/data/data";
+
+interface PageSelectProps {
+  page: number;
+  goToPage: (page: number) => void;
+}
+
+const PageSelect: React.FC<PageSelectProps> = ({ page, goToPage }) => {
+  const currentPagesLength = useAppSelector(selectPagesLength);
+
+  const pages = useMemo(
+    () => Array.from({ length: currentPagesLength }, (_, index) => index + 1),
+    [currentPagesLength]
+  );
+
+  const handlePageChange = (event: SelectChangeEvent<number>): void => {
+    goToPage(event.target.value as number);
+  };
+
+  return (
+    <FormControl sx={{ maxWidth: 100, px: 1 }} size="small">
+      <Select value={page} onChange={handlePageChange}>
+        {pages.map((page) => (
+          <MenuItem value={page}>{page}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default PageSelect;
