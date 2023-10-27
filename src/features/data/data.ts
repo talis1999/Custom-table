3acd81d69fb4610f45f5b68cd2c94ade2e0f009e
talis1999 <dantalisman@gmail.com>
2023-10-27 17:02:54 +0300

    adding filtering to redux store

--- src/features/data/data.ts
+++ src/features/data/data.ts
@@ -1,63 +1,76 @@
-import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit'
-import type { RootState } from '../../app/store';
+import { createSlice, PayloadAction, createSelector } from "@reduxjs/toolkit";
+import type { RootState } from "../../app/store";
 
-import { paginateRows } from './utils';
-
+import { paginateRows } from "./utils";
 
 export interface Row {
-    id: string;
-    [columnId: string]: any;
+  id: string;
+  [columnId: string]: any;
 }
 
 interface DataState {
-    rows: Row[];
-    searchQuery: string;
-    page: number;
-    limit: number;
+  rows: Row[];
+  searchQuery: string;
+  page: number;
+  limit: number;
 }
 
 const initialState: DataState = {
-    rows: [],
-    searchQuery: '',
-    page: 1,
-    limit: 25
-}
+  rows: [],
+  searchQuery: "Marry",
+  page: 1,
+  limit: 25,
+};
 
 export const dataSlice = createSlice({
-    name: 'data',
-    initialState,
-    reducers: {
-      setRows: (state, action: PayloadAction<Row[]>) => {
-        state.rows = [...action.payload];
-        
-      },
-      setLimit: (state, action: PayloadAction<number>) => {
-        state.page = 1;
-        state.limit = action.payload;
-      },
-      setPage: (state, action: PayloadAction<number>) => {
-        state.page = action.payload;
-      },
-    }
-  });
+  name: "data",
+  initialState,
+  reducers: {
+    setRows: (state, action: PayloadAction<Row[]>) => {
+      state.rows = [...action.payload];
+    },
+    setLimit: (state, action: PayloadAction<number>) => {
+      state.page = 1;
+      state.limit = action.payload;
+    },
+    setPage: (state, action: PayloadAction<number>) => {
+      state.page = action.payload;
+    },
+  },
+});
 
 export const { setRows, setLimit, setPage } = dataSlice.actions;
 
 const getRows = (state: RootState) => state.data.rows;
-
+export const getSearchQuery = (state: RootState) => state.data.searchQuery;
 export const getPage = (state: RootState) => state.data.page;
 export const getLimit = (state: RootState) => state.data.limit;
 
-// ! -- later on perhaps swith here the normal getRows to ones with filters + account special grouped rows and change the name to paginatedRows
-export const selectRows = createSelector(
-    [getRows, getPage, getLimit],
-    (rows, page, limit) => paginateRows({rows, page, limit})
-  );
+const selectFilteredRows = createSelector(
+  [getRows, getSearchQuery],
+  (rows, searchQuery) => {
+    if (!Boolean(searchQuery)) return rows;
+    const lowerCasedSearchQuery = searchQuery.toLowerCase();
+    return rows.filter((row) => {
+      return Object.keys(row).some((key) => {
+        if (key === "id" || typeof row[key] === "boolean") return false;
+        return row[key]
+          .toString()
+          .toLowerCase()
+          .includes(lowerCasedSearchQuery);
+      });
+    });
+  }
+);
 
-  // ! -- later on swith here the normal getRows to ones with filters + account special grouped rows 
 export const selectPagesLength = createSelector(
-  [getRows, getLimit],
+  [selectFilteredRows, getLimit],
   (rows, limit) => Math.ceil(rows.length / limit)
+);
+
+export const selectPaginatedRows = createSelector(
+  [selectFilteredRows, getPage, getLimit],
+  (rows, page, limit) => paginateRows({ rows, page, limit })
 );
 
 export default dataSlice.reducer;
